12c75feadc90ee4cc737495cff285682a0c0d163
Yelnurst <sailaubekov.yelnur@gmail.com>
2026-03-17 00:37:23 +0500

    Task 8: Implemented full CRUD with Class-Based Views for 3 models

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,7 +4,23 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
-    path('contact/', views.contact, name='contact'),
+
+    # Task 8
+    # CRUD для Прод
+    path('product/create/', views.ProductCreate.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', views.ProductUpdate.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', views.ProductDelete.as_view(), name='product_delete'),
+    
+    # CRUD для Категорий
+    path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
+    path('category/<int:pk>/update/', views.CategoryUpdate.as_view(), name='category_update'),
+    path('category/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),
+
+    # CRUD для Отзывов
+    path('review/create/', views.ReviewCreate.as_view(), name='review_create'),
+    path('review/<int:pk>/update/', views.ReviewUpdate.as_view(), name='review_update'),
+    path('review/<int:pk>/delete/', views.ReviewDelete.as_view(), name='review_delete'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Category, Product, Review
 
 def home(request):
     return render(request, 'mainapp/home.html')
@@ -19,3 +21,61 @@ def product_detail(request, product_id):
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
+
+
+
+
+# CRUD для Product
+class ProductCreate(CreateView):
+    model = Product
+    fields = '__all__' 
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list') 
+
+class ProductUpdate(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list')
+
+class ProductDelete(DeleteView):
+    model = Product
+    template_name = 'mainapp/generic_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+#CRUD для Category 
+class CategoryCreate(CreateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list')
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    fields = '__all__'
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list')
+
+class CategoryDelete(DeleteView):
+    model = Category
+    template_name = 'mainapp/generic_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+#CRUD дя Review
+class ReviewCreate(CreateView):
+    model = Review
+    fields = ['product', 'author', 'text']
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list')
+
+class ReviewUpdate(UpdateView):
+    model = Review
+    fields = ['text']
+    template_name = 'mainapp/generic_form.html'
+    success_url = reverse_lazy('product_list')
+
+class ReviewDelete(DeleteView):
+    model = Review
+    template_name = 'mainapp/generic_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
