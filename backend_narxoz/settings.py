8dafd3014c8a55492e05d633152321eff1f40516
Yelnurst <sailaubekov.yelnur@gmail.com>
2026-03-16 23:17:14 +0500

    Вариант-1(Task 2). Front and add pgAdmin

--- backend_narxoz/settings.py
+++ backend_narxoz/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend_narxoz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agro_db',          
+        'USER': 'postgres',     
+        'PASSWORD': '1234', 
+        'HOST': 'localhost',         
+        'PORT': '5432',              
     }
 }
 
