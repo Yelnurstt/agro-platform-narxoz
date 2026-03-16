a03505f7340663b6060b0926cdbcf530c3c4e2c4
Yelnurst <sailaubekov.yelnur@gmail.com>
2026-03-16 23:45:43 +0500

    Task 1 and 2. Миграция pgAdmin и рега моделей в админ панель

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Farmer, Tag, Product, Order, OrderItem, Review
 
-# Register your models here.
+#7 модел рега жасау, админ панельде көріну үшін
+admin.site.register(Category)
+admin.site.register(Farmer)
+admin.site.register(Tag)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Review)
